ecb91027e3b04ee8efa85cd44e98ed1202ed13c1
sanjay <sanjaysighania7@gmail.com>
2021-08-16 13:56:07 +0530

    React-Practice---2---Digital-Clock

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+function getcurrentTime(){
+    const currentdate= new Date();
+
+    return currentdate.toLocaleTimeString().toUpperCase();
+}
+
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            time:getcurrentTime(),
+        };
+
+    }
+    componentDidMount(){
+        this.timeinterval=setInterval(()=>{
+            this.state({time:getcurrentTime()})
+
+        },1000)
+
+    }
+    componentWillUnmount(){
+        clearInterval(this.timeinterval);
+    }
     render() {
 
         return(
-            <>
-               
-            </>
+            <div className="Clock">
+                <h1 id="time">{this.state.time}</h1>
+                
+            </div>
+            
         )
     }
 }
